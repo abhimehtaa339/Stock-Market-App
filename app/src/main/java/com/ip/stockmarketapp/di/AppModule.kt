554044bc4c96d9d5906c610aa6fa6b6675e337ec
abhimehtaa339 <abhimehtaa339@gmail.com>
2024-08-06 17:32:50 +0530

    Di Setup and Navigation setup complete

--- app/src/main/java/com/ip/stockmarketapp/di/AppModule.kt
+++ app/src/main/java/com/ip/stockmarketapp/di/AppModule.kt
@@ -1,11 +1,47 @@
 package com.ip.stockmarketapp.di
 
+import android.app.Application
+import androidx.room.Room
+import com.ip.stockmarketapp.data.local.StockDatabse
+import com.ip.stockmarketapp.data.remote.StockApi
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.create
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
+    @Provides
+    @Singleton
+    fun provideStockApi(): StockApi {
+        return Retrofit.Builder()
+            .baseUrl(StockApi.BASE_URL)
+            .addConverterFactory(MoshiConverterFactory.create())
+            .client(
+                OkHttpClient.Builder()
+                    .addInterceptor(HttpLoggingInterceptor().apply {
+                        level = HttpLoggingInterceptor.Level.BASIC
+                    }).build()
+            )
+            .build()
+            .create()
+    }
+
+    @Provides
+    @Singleton
+    fun provideStockDatabase(app: Application): StockDatabse {
+        return Room.databaseBuilder(
+            app,
+            StockDatabse::class.java,
+            "stockdb.db"
+        ).build()
+    }
+
 }
